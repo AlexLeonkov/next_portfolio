810f5ac582ff7acc52534ddeace634f7082107a1
ALEX LEONKOV <al@process-science.com>
2022-08-23 12:09:18 +0200

    buikd

--- src/screens/Food/FoodScreen.js
+++ src/screens/Food/FoodScreen.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Image from "next/image";
-
+import PhotosTemplate from "../../templates/PhotosTemplate";
 import { foodImages } from "../../../public/images/index";
 //import Header from "../layout/header";
 
 function food() {
   return (
-    <PhotosTemplate images={foodsImages} header="Food Photography Berlin" />
+    <PhotosTemplate images={foodImages} header="Food Photography Berlin" />
   );
 }
 
