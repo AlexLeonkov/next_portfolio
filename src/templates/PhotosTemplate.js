9b42fd74906f5d30e5bf0b474ce6d8149faea571
ALEX LEONKOV <al@process-science.com>
2022-08-23 16:05:53 +0200

    styling

--- src/templates/PhotosTemplate.js
+++ src/templates/PhotosTemplate.js
@@ -1,30 +1,31 @@
 import React from "react";
 import Image from "next/image";
 import Header from "../components/Header";
+import styles from "./PhotosTemplate.module.css";
 
 function photosRender({ images, header }) {
   return (
     <div>
       <Header />
 
+      <h1 className={styles.mainTitle}>{header}</h1>
+
       <main>
-        <div className="row">
+        <div className={styles.container}>
           {images.map((image, index) => {
             return (
-              <div key={index} className="column">
+              <div key={index} className={styles.column}>
                 <Image
                   priority={true}
                   src={image}
                   alt={index}
-                  width={650}
-                  height={650}
+                  width={1000}
+                  height={1000}
                 />
               </div>
             );
           })}
         </div>
-
-        <h1 className="main_title">{header}</h1>
       </main>
     </div>
   );
